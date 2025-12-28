13ad0fd6c77011b0efe4b963fd0d89972e5386d8
h108777 <harshiluttaradhi18@gmail.com>
2025-12-29 01:30:09 +0530

    The user version is ready

--- GuessTheNumber/main.py
+++ GuessTheNumber/main.py
@@ -1,9 +1,13 @@
 import random
-def guess(x):
+def guess(x):#the user is guessing the computer generated number 
     random_number=random.randint(1,x)
     guess =0
     while guess!=random_number:
-        guess=input(f"Guess a number between 1 and {x}.")
+        guess=int(input(f"Guess a number between 1 and {x}."))
         print(guess)
+        if guess<random_number:
+            print("Try again. Too Low.")
+        elif guess>random_number:
+            print("Try again. Too high")
+    print(f"Yay you have guessed the number {random_number} correctly!!")
 
-guess(10)
