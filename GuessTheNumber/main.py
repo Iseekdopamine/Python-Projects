e2e708c522c52d9010d861ec33b63036158e6aef
h108777 <harshiluttaradhi18@gmail.com>
2025-12-29 02:03:27 +0530

    The game is ready

--- GuessTheNumber/main.py
+++ GuessTheNumber/main.py
@@ -11,3 +11,30 @@ def guess(x):#the user is guessing the computer generated number
             print("Try again. Too high")
     print(f"Yay you have guessed the number {random_number} correctly!!")
 
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+
+        feedback = input(
+            f'Is {guess} too high (H), too low (L), or correct (C)? '
+        ).lower()
+
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f"Yayy!! The computer guessed your number, {guess}, correctly!")
+
+
+computer_guess(10)
+
+    
+
