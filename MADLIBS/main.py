743330389f4b47b6083dc7ca31b5044e424684ab
h108777 <harshiluttaradhi18@gmail.com>
2025-12-28 22:20:05 +0530

    Core logic of MADLIBS is string concatenation

--- MADLIBS/main.py
+++ MADLIBS/main.py
@@ -1,1 +1,8 @@
 print("project has started")
+#project is based on the implementation of string concatenation 
+
+#ways to go around it 
+youtuber="Harsh Sinha"
+print("Subscribe to "+youtuber)
+print("Subscribe to {}".format(youtuber))
+print(f"Suscribe to {youtuber}")#cleanest way 
