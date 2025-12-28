884a8ce91f8955d117b2e451c1039e24b7b1f9f1
h108777 <harshiluttaradhi18@gmail.com>
2025-12-28 22:36:00 +0530

    building basic code

--- MADLIBS/main.py
+++ MADLIBS/main.py
@@ -1,8 +1,16 @@
-print("project has started")
+#print("project has started")
 #project is based on the implementation of string concatenation 
 
 #ways to go around it 
-youtuber="Harsh Sinha"
-print("Subscribe to "+youtuber)
-print("Subscribe to {}".format(youtuber))
-print(f"Suscribe to {youtuber}")#cleanest way 
+#youtuber="Harsh Sinha"
+#print("Subscribe to "+youtuber)
+#print("Subscribe to {}".format(youtuber))
+#print(f"Suscribe to {youtuber}")#cleanest way 
+adj = input("Adjective: ")
+verb1= input("Verb: ")
+verb2= input("Verb: ")
+famous_person= input("Famous Person: ")
+madlib = f"I am so {adj} but I gotta keep {verb1}! I love to {verb2}. \
+Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
