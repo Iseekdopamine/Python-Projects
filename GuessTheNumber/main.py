59e589839b36be423bd5f99c4090e01b8ba0c7e3
h108777 <harshiluttaradhi18@gmail.com>
2025-12-29 01:22:29 +0530

    basic input output to ensure if the logic is okay

--- GuessTheNumber/main.py
+++ GuessTheNumber/main.py
@@ -1,1 +1,9 @@
-print("new project has started")
+import random
+def guess(x):
+    random_number=random.randint(1,x)
+    guess =0
+    while guess!=random_number:
+        guess=input(f"Guess a number between 1 and {x}.")
+        print(guess)
+
+guess(10)
