e7fa6597c784997e463c7fc263393725f97739a5
h108777 <harshiluttaradhi18@gmail.com>
2025-12-28 23:23:29 +0530

    Add resolutions madlib to sample_madlibs folder

---
+++ sample_madlibs/resolutions.py
@@ -0,0 +1,25 @@
+
+def madlib():
+    adj1={"Adjective: "}
+    adj2={"Adjective: "}
+    adj3={"Adjective: "}
+    adj4={"Adjective: "}
+    verb1={"Verb: "}
+    verb2={"Verb: "}
+    verb3={"Verb: "}
+    verb4={"Verb: "}
+    word={"Word: "}
+
+
+
+    madlib=f"I will be my {adj1} this year. I only allow {adj2} energies this year \
+     I will {verb1} daily. I will {verb2} daily. I will {verb3} daily. I will \
+        {verb4} daily. Life is not {adj3}. Everything will be {adj4} in the \
+             {word}."
+
+    print(madlib)
+
+
+
+
+
